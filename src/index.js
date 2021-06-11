391c7f2378e13d54aa8cdc3427ca45b6fba63a14
Ezekiel <dolapo@Dolapos-MacBook-Pro.local>
2021-06-11 11:10:28 +0100

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -2,35 +2,26 @@
 //Adam Hartleb
 //How do I hook up the task-manager API's authentication to a frontend?
 
-const express = require('express')
-require('./db/mongoose')
-
+const express = require("express");
+require("./db/mongoose");
 
 //user router
-const userRouter = require('./routers/user')
+const userRouter = require("./routers/user");
 //task router
-const taskRouter = require('./routers/task')
+const taskRouter = require("./routers/task");
 
-const app = express()
-const port = process.env.PORT
-
+const app = express();
+const port = process.env.PORT;
 
 //configure express to parse incoming json
-app.use(express.json())
+app.use(express.json());
 
 //register our router with app
-app.use(userRouter)
+app.use(userRouter);
 
 //register our router
-app.use(taskRouter)
-
-
-
+app.use(taskRouter);
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
-})
-
-
-
-
+  console.log(`Server running on port ${port}`);
+});
